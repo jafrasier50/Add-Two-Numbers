c17f92f9e72b1aa12ceb081d3ed4dc885f11baba
Joshua Frasier <jafrasier50@hotmail.com>
2018-05-01 14:33:55 -0500

    Assigment submission

---
+++ add.py
@@ -0,0 +1,4 @@
+first_number = str(input("Pick a number: "))
+second_number = str(input("Pick a second number to add: "))
+sum = float(first_number) + float(second_number)
+print(f"The numbers {first_number} and {second_number} added together total as {sum}.")
